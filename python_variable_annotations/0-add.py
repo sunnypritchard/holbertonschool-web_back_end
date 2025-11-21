3a06b11c29b07a0e2e5bb146a8765b429e639a7c
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-21 11:26:02 +0100

    style:  E501 line too long (88 > 79 characters)

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""This module contains a function that adds two float
+numbers with type annotations."""
+
+
 def add(a: float, b: float) -> float:
+    """Returns the sum of two float numbers."""
     return a + b
