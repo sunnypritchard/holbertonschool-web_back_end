1bfa088ea393f22d284677d41700047baab8a128
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-21 11:45:17 +0100

    return the floor of a float number as int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that floors a float
+number with type annotations."""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float number as an integer."""
+    return int(n // 1)
