728bed9ed692d9ae98287c3abdbb0a5d7ea99cc8
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-21 11:37:35 +0100

    concatenate two strings with type annotations and return the concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenates two strings
+with type annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
