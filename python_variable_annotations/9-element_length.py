2f7cbc8b86662874749f32b53a1000432e717f79
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-21 14:04:43 +0100

    returns a list of tuples with each sequence and its length

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""This module contains a function that takes a list of tuples
-and returns a list of their lengths with type annotations."""
+"""This module contains a function that takes a list of sequences
+and returns a list of tuples with each sequence and its length."""
 
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[Tuple]) -> List[int]:
-    """Returns a list of lengths of the tuples in the input list."""
-    return [len(i) for i in lst]
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples with each sequence and its length."""
+    return [(i, len(i)) for i in lst]
