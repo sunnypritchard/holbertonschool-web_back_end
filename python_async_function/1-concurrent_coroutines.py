bda3eac48c06605c8ad3024198b024025040853c
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-24 14:02:09 +0100

    fix: annotate variables

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -16,8 +16,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: List of all the delays in ascending order.
     """
-    delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
+    tasks: List[asyncio.Task] = [wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
         delay = await task
