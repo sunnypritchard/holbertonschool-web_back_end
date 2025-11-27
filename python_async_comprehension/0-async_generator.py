a6fe2cccba7162890073cc9b05374a745a94d6e2
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-27 14:06:05 +0100

    async generator that yield 10 randoms numbers

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module that contains an asynchronous generator function"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import float
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> float:
     """Asynchronous generator that yields 10 random numbers"""
     for i in range(10):
         await asyncio.sleep(1)
