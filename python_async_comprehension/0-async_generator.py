61602a5270902de434000dd921da5d287c4c65ac
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-27 14:33:13 +0100

    fix: add type annotation for return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module that contains an asynchronous generator function"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that yields 10 random numbers"""
     for i in range(10):
         await asyncio.sleep(1)
