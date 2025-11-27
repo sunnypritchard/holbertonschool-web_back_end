3fe43286c65f15eb03357c4b105cafbc348572d4
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-27 15:08:29 +0100

    collect 10 random numbers using async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using async comprehension"""
-    async_list = []
-    async for i in async_generator():
-        async_list.append(i)
-    return async_list
+    return [i async for i in async_generator()]
