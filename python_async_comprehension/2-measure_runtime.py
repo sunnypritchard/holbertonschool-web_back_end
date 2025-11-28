19ea89af5e1d2553eb960c611f69fdc52a05efe0
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-28 09:46:18 +0100

    fix: use asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module that contains a function to measure runtime of async_comprehension"""
+import asyncio
 import time
 
 
@@ -9,6 +10,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension"""
     start_time = time.time()
-    await async_comprehension()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
     return end_time - start_time
