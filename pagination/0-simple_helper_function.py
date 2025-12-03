3915aed3b3f34e016c0fedf5828306e519cf8bbb
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-03 12:10:17 +0100

    fix(task 0): add args hint type int

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -11,6 +11,6 @@ def index_range(page: int, page_size: int) -> tuple:
         tuple: A tuple containing the start index and end index.
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
 
     return (start_index, end_index)
