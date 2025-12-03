48cb7ba9fa74407ef33cda770f1459999ce6d881
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-03 11:57:59 +0100

    feat(task 0): returns a tuple of star index and end index for pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -11,17 +11,7 @@ def index_range(page, page_size) -> Tuple[int, int]:
     Returns:
         tuple: A tuple containing the start index and end index.
     """
-    start_index: int = (page - 1) * page_size
-    end_index: int = page * page_size
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
 
     return (start_index, end_index)
-
-
-if __name__ == "__main__":
-    res = index_range(1, 7)
-    print(type(res))
-    print(res)
-
-    res = index_range(page=3, page_size=15)
-    print(type(res))
-    print(res)
