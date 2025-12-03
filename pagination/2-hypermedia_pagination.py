45c4b450c2f01cb84867bd94640fcd4f46853497
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-03 16:35:04 +0100

    feat(task 2): get hypermedia pagination information

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -72,15 +72,3 @@ class Server:
         }
 
         return hypermedia_info
-    
-
-if __name__ == "__main__":
-    server = Server()
-
-    print(server.get_hyper(1, 2))
-    print("---")
-    print(server.get_hyper(2, 2))
-    print("---")
-    print(server.get_hyper(100, 3))
-    print("---")
-    print(server.get_hyper(3000, 100))
