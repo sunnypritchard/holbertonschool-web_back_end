bbc9076843bbd7937271717ab004381943aaa011
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-03 12:05:42 +0100

    fix(task 0): add return hint type tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -11,6 +11,6 @@ def index_range(page, page_size) -> tuple:
         tuple: A tuple containing the start index and end index.
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
 
     return (start_index, end_index)
