a7c383ac82de123517f349a8f8d7fcce17965896
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-05 22:35:40 +0100

    feat(task 10): Update topics of school document based on the name

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,18 +3,8 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """Update topics of school document based on the name
-
-    Args:
-        mongo_collection: The MongoDB collection object.
-        name: The name of the document to update.
-        topics: A list of topics to set for the document.
-
-    Returns:
-        The result of the update operation.
-    """
-    update_result = mongo_collection.update_one(
+    """Update topics of school document based on the name."""
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
-    return update_result.modified_count
