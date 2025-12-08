11d2e685248f736cc9d715a2f3364971ec6f48f8
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-08 15:22:00 +0100

    feat(task 10): for...of operator

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
   }
 
-  return array;
+  return result;
 }
