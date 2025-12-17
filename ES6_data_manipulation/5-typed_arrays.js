ae6866814a6085c160f74722d93dd59e986ebffe
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-17 13:01:19 +0100

    feat(task 5): add function to create Int8 typed array with ArrayBuffer
    - Create ArrayBuffer of specified length
    - Use DataView to set Int8 value at position
    - Throw error when position is outside range

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const int8Array = new Int8Array(length);
-  int8Array[position] = value;
-  return int8Array;
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8View = new DataView(buffer);
+  int8View.setInt8(position, value);
+  return buffer;
 }
