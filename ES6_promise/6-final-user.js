72897350749d4083a614fecd66a256f95f041bf0
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-19 18:48:02 +0100

    feat(task 6): add handleProfileSignup using Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,5 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    uploadPhoto(fileName)
   ])
