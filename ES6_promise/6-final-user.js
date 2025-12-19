125684ab5653579714e49f14081253a7fb777410
Sunny Pritchard <pritchardsunny@gmail.com>
2025-12-19 18:53:59 +0100

    Delete ES6_promise/6-final-user.js

--- ES6_promise/6-final-user.js
+++
@@ -1,9 +0,0 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default function handleProfileSignup (firstName, lastName, fileName) {
-    return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-    ]);
-}
